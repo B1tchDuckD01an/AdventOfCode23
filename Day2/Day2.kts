c5423b0a07ca6f6b78f4c5d849e839f428a7c098
ElaineDore-OS <elaine.dore@os.uk>
2023-12-04 11:53:41 +0000

    day 2

--- Day2/Day2.kts
+++ Day2/Day2.kts
@@ -1,25 +1,42 @@
 import java.io.File
-val lines = File("test.txt").readLines()
+val lines = File("input.txt").readLines()
 
-/*
-class Game(id: Int){
-
-
-}
-*/
-
-fun parseGames(strings: List<String>){
-    //var games = mutableListOf<Game>()
-    strings.forEach {
-        val id = it.split(":")[0].split(" ")[1]
-        println(id)
-        val rounds = it.split(":")[1].split(";")[0]
-        rounds.split(",").forEach{
-            it.split(" ").forEach {
-                println(it)
-            }
-        }
-        println(rounds)
+fun hasHigherValue(pairs: List<Pair<String, Int>>, globalValues: Map<String, Int>): Boolean {
+    return pairs.any { (key, value) ->
+        globalValues[key]?.let { globalValue ->
+            value > globalValue
+        } ?: false
     }
 }
-parseGames(lines)
+
+val blocks = mapOf(
+    "red" to 12,
+    "green" to 13,
+    "blue" to 14
+)
+
+fun parseGames(strings: List<String>) : MutableMap<Int,Boolean> {
+    var possibilities = mutableMapOf<Int,Boolean>()
+
+    strings.forEach {
+        //games and make all games false
+        val id = it.split(":")[0].split(" ")[1].toInt()
+        possibilities[id] = true
+
+        //rounds
+        val rounds = it.split(":")[1].split(";")
+        for(round in rounds) {
+            //blocks
+            val pairs = round.split(",").map {
+                Pair(it.split(" ")[2], it.split(" ")[1].toInt())
+            }
+            if(hasHigherValue(pairs, blocks))
+                possibilities[id] = false
+        }
+    }
+    return possibilities
+}
+
+println("part 1 ")
+println(parseGames(lines).filter{ it.value == true}.keys.sum())
+
