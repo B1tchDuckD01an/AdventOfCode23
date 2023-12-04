482a6bbe62b6c92d8df1181bcd69374a1ee1f850
ElaineDore-OS <elaine.dore@os.uk>
2023-12-04 12:19:38 +0000

    day 2 block block boom boom

--- Day2/Day2.kts
+++ Day2/Day2.kts
@@ -1,4 +1,5 @@
 import java.io.File
+
 val lines = File("input.txt").readLines()
 
 fun hasHigherValue(pairs: List<Pair<String, Int>>, globalValues: Map<String, Int>): Boolean {
@@ -15,7 +16,7 @@ val blocks = mapOf(
     "blue" to 14
 )
 
-fun parseGames(strings: List<String>) : MutableMap<Int,Boolean> {
+fun parseGamesPart1(strings: List<String>) : MutableMap<Int,Boolean> {
     var possibilities = mutableMapOf<Int,Boolean>()
 
     strings.forEach {
@@ -37,6 +38,29 @@ fun parseGames(strings: List<String>) : MutableMap<Int,Boolean> {
     return possibilities
 }
 
-println("part 1 ")
-println(parseGames(lines).filter{ it.value == true}.keys.sum())
+fun parseGamesPart2(strings: List<String>) : Int{
+    var possibilities = 0
 
+
+    strings.forEach {
+        //rounds
+        val rounds = it.split(":")[1].split(";")
+        var blockmap = mutableMapOf("red" to 0, "blue" to 0, "green" to 0)
+        for(round in rounds) {
+            round.split(",").map {
+                Pair(it.split(" ")[2], it.split(" ")[1].toInt())}.forEach{
+                val a = blockmap[it.first] ?: 0
+                    if(a < it.second)
+                        blockmap[it.first] = it.second
+            }
+        }
+        possibilities += blockmap.values.reduce { acc, i -> acc * i}
+    }
+    return possibilities
+}
+
+println("part 1 ")
+println(parseGamesPart1(lines).filter{ it.value == true}.keys.sum())
+
+println("part 2 ")
+println(parseGamesPart2(lines))
